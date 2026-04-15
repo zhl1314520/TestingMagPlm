058d0d69b7b21c9a4fce16937f39bf1d07c8769a
developer <17201665342@163.com>
2026-04-15 17:04:50 +0800

    V4.15.1

--- frontend/src/Dashboard/Bugs.vue
+++ frontend/src/Dashboard/Bugs.vue
@@ -170,10 +170,18 @@
           <div class="bug-id">#{{ bug.id }}</div>
           <div class="action-buttons" @click.stop>
             <button @click="editBug(bug)" class="btn-edit" title="编辑">
-              <Icon icon="carbon:edit" width="16" height="16" />
+              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
+                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
+              </svg>
             </button>
             <button @click="deleteBug(bug.id)" class="btn-delete" title="删除">
-              <Icon icon="carbon:trash-can" width="16" height="16" />
+              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <polyline points="3 6 5 6 21 6"/>
+                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
+                <line x1="10" y1="11" x2="10" y2="17"/>
+                <line x1="14" y1="11" x2="14" y2="17"/>
+              </svg>
             </button>
           </div>
         </div>
